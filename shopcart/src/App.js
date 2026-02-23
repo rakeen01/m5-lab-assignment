3eb528773366aa78a4175b0639efe5a932900b8d
Md Rakeen Murtaza <rakeen.murtaza.12@gmail.com>
2026-02-23 07:29:01 -0800

    added images

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    siteName: "Shop to React",
+    products: [
+      { image: "/products/cologne.jpg", desc: "Unisex Cologne", value: 0 },
+      { image: "/products/iwatch.jpg", desc: "Apple iWatch", value: 0 },
+      { image: "/products/mug.jpg", desc: "Unique Mug", value: 0 },
+      { image: "/products/wallet.jpg", desc: "Mens Wallet", value: 0 },
+    ],
+  };
+
+  render() {
+    const totalQty = this.state.products.reduce(
+      (sum, p) => sum + p.value,
+      0
+    );
+
+    return (
+      <div style={{ padding: 40 }}>
+        <h1>{this.state.siteName}</h1>
+        <p>{totalQty} items</p>
+
+        {this.state.products.map((p, i) => (
+          <div key={i} style={{ marginTop: 20 }}>
+            <h3>{p.desc}</h3>
+            <img src={p.image} alt={p.desc} width={120} />
+            <div>Quantity: {p.value}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
