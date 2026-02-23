2e7905672385184c8ded249bebb92a6914baaae0
Md Rakeen Murtaza <rakeen.murtaza.12@gmail.com>
2026-02-23 07:31:11 -0800

    return block edits

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -18,17 +18,31 @@ class App extends Component {
     );
 
     return (
-      <div style={{ padding: 40 }}>
-        <h1>{this.state.siteName}</h1>
-        <p>{totalQty} items</p>
+      <div style={{ backgroundColor: "#f3f3f3", minHeight: "100vh" }}>
+        
+        {/* Header */}
+        <div
+          style={{
+            backgroundColor: "#63C6E8",
+            padding: "20px 40px",
+            borderBottom: "1px solid #ddd",
+          }}
+        >
+          <h1 style={{ margin: 0 }}>{this.state.siteName}</h1>
+          <div style={{ fontSize: 14 }}>{totalQty} items</div>
+        </div>
 
-        {this.state.products.map((p, i) => (
-          <div key={i} style={{ marginTop: 20 }}>
-            <h3>{p.desc}</h3>
-            <img src={p.image} alt={p.desc} width={120} />
-            <div>Quantity: {p.value}</div>
-          </div>
-        ))}
+        {/* Product list */}
+        <div style={{ padding: 40 }}>
+          {this.state.products.map((p, i) => (
+            <div key={i} style={{ marginBottom: 40 }}>
+              <h3>{p.desc}</h3>
+              <img src={p.image} alt={p.desc} width={120} />
+              <div>Quantity: {p.value}</div>
+            </div>
+          ))}
+        </div>
+
       </div>
     );
   }
